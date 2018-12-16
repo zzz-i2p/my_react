d161fabd1dd6e387c719d5f9fba7bd2f70ef0035
zzz <z@myzzz.org>
2018-12-16 07:21:42 +0000

    Updates

---
+++ precache-manifest.863f4ea071d443a9baa780d11d3b88fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8714641d7a4e4522beae",
+    "url": "/my_react/static/js/runtime~main.8714641d.js"
+  },
+  {
+    "revision": "3b17bf282ecad8606caa",
+    "url": "/my_react/static/js/main.3b17bf28.chunk.js"
+  },
+  {
+    "revision": "4f1c92d69e45b5c67b3f",
+    "url": "/my_react/static/js/1.4f1c92d6.chunk.js"
+  },
+  {
+    "revision": "3b17bf282ecad8606caa",
+    "url": "/my_react/static/css/main.c5d8c947.chunk.css"
+  },
+  {
+    "revision": "9630873f46138092164bd7f45775d639",
+    "url": "/my_react/index.html"
+  }
+];
